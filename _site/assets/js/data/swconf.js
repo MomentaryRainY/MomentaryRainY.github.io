a698031b42bfbebdd2343e965e7831c54ea42b99
MomentaryRainY <160746168+MomentaryRainY@users.noreply.github.com>
2024-02-28 13:58:14 +0800

    post animation

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1709096835',resources: [
+    cacheName: 'chirpy-1709099844',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
