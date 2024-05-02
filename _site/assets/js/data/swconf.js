914b03505572b601bcf92111cafe61f05d6d4231
MomentaryRainY <160746168+MomentaryRainY@users.noreply.github.com>
2024-05-02 16:16:32 +0800

    post test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1714635679',resources: [
+    cacheName: 'chirpy-1714637571',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
