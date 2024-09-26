8437416cd8fac0b07d2e3e76ebaf629f54e056c6
MomentaryRainY <160746168+MomentaryRainY@users.noreply.github.com>
2024-09-27 00:33:54 +0800

    modify displaying

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1727367725',resources: [
+    cacheName: 'chirpy-1727368274',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
