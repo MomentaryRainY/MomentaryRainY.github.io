dab5750689676c204c7a6973f3de265f770c3f10
MomentaryRainY <160746168+MomentaryRainY@users.noreply.github.com>
2024-09-27 00:26:10 +0800

    delete canvas

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1714640777',resources: [
+    cacheName: 'chirpy-1727367725',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
