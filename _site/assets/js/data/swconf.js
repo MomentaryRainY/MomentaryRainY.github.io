cbb2c45f2765638b0194c12e91d6a55f0438dbed
MomentaryRainY <160746168+MomentaryRainY@users.noreply.github.com>
2024-09-27 13:00:57 +0800

    animation modified

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1727370524',resources: [
+    cacheName: 'chirpy-1727411432',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
