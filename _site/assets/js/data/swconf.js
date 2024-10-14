d0e3b3eabdbf20c2158152d48bcb022697ccaba0
MomentaryRainY <160746168+MomentaryRainY@users.noreply.github.com>
2024-10-14 12:49:34 +0800

    correct 404

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728881159',resources: [
+    cacheName: 'chirpy-1728881342',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
